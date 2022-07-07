8854c2b5541c84a17edca88524fe6c188cfb3d92
Dmitry286 <dmishi@live.spbstu.ru>
2022-07-07 23:50:31 +0300

    Hot keys done. Serch method and form done.

--- Controller.py
+++ Controller.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import sqlalchemy
 import psycopg2
 
@@ -43,9 +45,10 @@ def find_employee_by_name(employee_name: str) -> ...:
         print(emp)
     return stmt
 
-def query_find_employee_by_name(search_name: str) -> ...: #todo какое должно быть возвращаемое значение
-    results = CONNECT_SESSION.query(Employee).filter_by(name='ed').all()
-    return results
+def query_find_employee_by_name(search_name: str) -> List: #todo какое должно быть возвращаемое значение
+    result = CONNECT_SESSION.query(Employee).filter_by(name=search_name).all()
+
+    return [x.to_dict() for x in result]
 
 def query_find_by_id(entity, id) -> ...: #todo какое должно быть возвращаемое значение
     obj = CONNECT_SESSION.query(entity).get(id)
